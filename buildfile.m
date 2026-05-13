3c599b27604b69ff1974a67d7064238bc4653f76
Bensingh Pancras <bpancras@mathworks.com>
2026-05-13 20:26:08 +0530

    Added a buildfile to create mex functions

---
+++ buildfile.m
@@ -0,0 +1,12 @@
+function plan = buildfile
+plan = buildplan;
+files = ["det2x2.c", "det3x3.c", "ft_getopt.c", "ft_spike_sub_crossx.c", "getpid.c", "inv2x2.c", "inv3x3.c",...
+  "meg_leadfield1.c", "mtimes2x2.c", "mtimes3x3.c", "mxDeserialize_c.c", "mxSerialize_c.c", "nanaccum.c", "nanmean.c",...
+   "nanstd.c", "nansum.c", "nanvar.c", "plgndr.c", "read_16bit.c", "read_24bit.c", "read_ctf_shm.c", "rename.c",...
+  "sandwich2x2.c", "sandwich3x3.c", "write_ctf_shm.c"];
+srcFiles = matlab.buildtool.io.Glob("src/det3x3.c");
+mexOptions=["-I./src"];
+outputDir= fullfile("connectivity", "private");
+for f = files
+plan("mex:"+f) = matlab.buildtool.tasks.MexTask(fullfile("src",files(1)), outputDir, Options=mexOptions);
+end
