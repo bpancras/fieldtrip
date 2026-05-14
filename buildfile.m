58e1ae5183373e9aa31675cc088932a42169a407
bpancras <bpancras@mathworks.com>
2026-05-14 14:32:31 +0530

    removed the file glob

--- buildfile.m
+++ buildfile.m
@@ -4,7 +4,6 @@ files = ["det2x2.c", "det3x3.c", "ft_getopt.c", "ft_spike_sub_crossx.c", "getpid
   "meg_leadfield1.c", "mtimes2x2.c", "nanmean.c",...
    "nanstd.c", "nansum.c", "nanvar.c", "plgndr.c", "read_16bit.c", "read_24bit.c", "read_ctf_shm.c", "rename.c",...
   "sandwich2x2.c", "sandwich3x3.c", "write_ctf_shm.c"];
-srcFiles = matlab.buildtool.io.Glob("src/det3x3.c");
 outputDir= fullfile("connectivity", "private");
 for f = files
 plan("mex:"+f) = matlab.buildtool.tasks.MexTask(fullfile("src",f), outputDir);
